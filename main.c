1e1f8d6809fbaec7b2118bf6c57fc66edf2bdc3c
Ricardo Dominguez Olmedo <riddoddo@gmail.com>
2018-02-19 19:27:57 +0000

    Update main.c

--- main.c
+++ main.c
@@ -50,10 +50,8 @@ TIM_HandleTypeDef htim8;
 //Global heartbeats
 uint32_t globalHeartbeat_50us = 0, heartbeat_100us = 0, heartbeat_1ms = 0, heartbeat_10ms = 0;
 int measuredSpeed = 0, demandedSpeed = 0, int speedError = 0;
-int demandedPWM = 0; //Duty cycle proportional to the control 
+int demandedPWM = 0, controlOutput = 0; //Duty cycle proportional to the control 
 uint16_t accelPedalValue_scaled = 0;
-int controlOutput = 0;
-int speedError = 0;
 int encoder_ticks = 0;
 int real_ticks = 0;
 
